48ccd2e27e1cfc565ceb5663b1590ac2b8765bd9
Manup <314.developer@gmail.com>
2024-09-16 22:12:35 +0530

    Update contact_me.php

--- php/contact_me.php
+++ php/contact_me.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 $to = "manup.rav@gmail.com";
 $subject = "Message From Revolve 314 Site";
 $message = '
